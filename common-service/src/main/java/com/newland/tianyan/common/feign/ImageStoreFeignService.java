1f1536ff598b9f15d006b89051c49e1b7626c34f
huangluojia <huanglj@newland.com.cn>
2021-02-07 15:30:25 +0800

    注册中心添加namespace属性

--- common-service/src/main/java/com/newland/tianyan/common/feign/ImageStoreFeignService.java
+++ common-service/src/main/java/com/newland/tianyan/common/feign/ImageStoreFeignService.java
@@ -1,9 +1,14 @@
 package com.newland.tianyan.common.feign;
 
+import com.newland.tianyan.common.feign.dto.imageStore.DownloadReq;
+import com.newland.tianyan.common.feign.dto.imageStore.DownloadRes;
+import com.newland.tianyan.common.feign.dto.imageStore.UploadReq;
+import com.newland.tianyan.common.feign.dto.imageStore.UploadRes;
 import com.newland.tianyan.common.feign.falback.FeignConfiguration;
 import com.newland.tianyan.common.feign.falback.ImageServiceFeignClientFallbackImpl;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author: RojiaHuang
@@ -13,11 +18,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name = "image-store-service", fallback = ImageServiceFeignClientFallbackImpl.class,
         configuration = FeignConfiguration.class)
 public interface ImageStoreFeignService {
+    String URI_PREFIX = "/backend/image";
 
-    String uploadImage(String image);
+    @PostMapping(URI_PREFIX + "/uploadV2")
+    UploadRes uploadV2(@RequestBody UploadReq uploadReq);
 
-    String downloadImage(String imagePath);
-
-    String uploadImageV2(String image);
-
+    @PostMapping(URI_PREFIX + "/download")
+    DownloadRes download(@RequestBody DownloadReq downloadReq);
 }
