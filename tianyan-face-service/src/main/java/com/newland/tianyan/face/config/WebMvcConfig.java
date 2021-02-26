f7132a66e6dc5b39b76b10aafc30e62bd4163395
huangluojia <huanglj@newland.com.cn>
2021-02-26 13:36:29 +0800

    节点增加日志+image模块补充下划线json模式

---
+++ tianyan-face-service/src/main/java/com/newland/tianyan/face/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.newland.tianyan.face.config;
+
+import com.newland.tianyan.common.log.ApiMethodLogIntercept;
+import com.newland.tianyan.common.log.LogFixColumnsHelper;
+import com.newland.tianyan.common.log.ServerAddressHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author: RojiaHuang
+ * @description:
+ * @date: 2021/2/25
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        //argumentResolvers.add(new ApiReqArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ApiMethodLogIntercept(logFixColumnsHelper, serverAddressHelper));
+    }
+
+    @Autowired
+    private LogFixColumnsHelper logFixColumnsHelper;
+    @Autowired
+    private ServerAddressHelper serverAddressHelper;
+}
