ecaa88cc4867105f47f8a11ad20256ffb8504a72
huangluojia <huanglj@newland.com.cn>
2021-03-16 16:26:33 +0800

    rebuild

--- tianyan-common/src/main/java/com/newland/tianyan/common/exception/NotFoundException.java
+++ tianyan-common/src/main/java/com/newland/tianyan/common/exception/NotFoundException.java
@@ -1,38 +1,42 @@
-//package com.newland.tianyan.common.exception;
-//
-//import com.newland.tianyan.common.constans.GlobalArgumentErrorEnums;
-//import com.newland.tianyan.common.exception.ArgumentException;
-//import org.slf4j.MDC;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-///**
-// * @author: RojiaHuang
-// * @description:
-// * @date: 2021/3/6
-// */
-//@Controller
-//public class NotFoundException implements ErrorController {
-//
-//    @Override
-//    public String getErrorPath() {
-//        return "/error";
-//    }
-//
-//    @RequestMapping(value = {"/error"})
-//    @ResponseBody
-//    public Object error(HttpServletRequest request) {
-//        Map<String, Object> body = new HashMap<>();
-//        ArgumentException exception = GlobalArgumentErrorEnums.INVALID_METHOD.toException();
-//        body.put("log_id", MDC.get("traceId"));
-//        body.put("error_code", exception.getErrorCode());
-//        body.put("error_msg", exception.getErrorMsg());
-//        return body;
-//    }
-//}
+package com.newland.tianyan.common.exception;
+
+import com.newland.tianyan.common.constans.GlobalArgumentErrorEnums;
+import org.slf4j.MDC;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: RojiaHuang
+ * @description:
+ * @date: 2021/3/6
+ */
+@Controller
+public class NotFoundException extends BasicErrorController {
+
+    public NotFoundException(ErrorAttributes errorAttributes) {
+        super(errorAttributes, new ErrorProperties());
+    }
+
+    @Override
+    @RequestMapping
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest httpServletRequest) {
+        Map<String, Object> body = new HashMap<>();
+        ArgumentException exception = GlobalArgumentErrorEnums.INVALID_METHOD.toException();
+        body.put("logId", MDC.get("traceId"));
+        body.put("errorCode", exception.getErrorCode());
+        body.put("errorMsg", exception.getErrorMsg());
+        return new ResponseEntity<>(
+                body
+                , HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
